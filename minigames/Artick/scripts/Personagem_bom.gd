c6be5a95cae6a38ab15e60a4a7cdb66d86c7d7f2
Gabriel de Freitas Garcia <gabrielfreitasgarcia@globo.com>
2018-10-08 21:52:40 -0300

    Artick
    Grandes modificações no Artick, removi o Gandhi

--- minigames/Artick/scripts/Personagem_bom.gd
+++ minigames/Artick/scripts/Personagem_bom.gd
@@ -1,10 +1,43 @@
 extends KinematicBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+const speed = 1000
+const normal = Vector2(0,-1)
+const gravidade = 10
+
+var velocity = Vector2(0,0)
+var pos_atual
+var giro
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
+	set_process(true)
+	pass
+
+func _process(delta):
+	giro = Input.get_gyroscope()
+	if is_move_and_slide_on_floor():
+		velocity.y = 0
+		if giro.y >0:
+			if velocity.x > 0:
+				velocity.x += speed*delta
+			elif velocity.x == 0:
+				velocity.x = 500
+			else:
+				velocity.x = 500
+		elif giro.y < 0:
+			if velocity.x < 0:
+				velocity.x -= speed*delta
+			elif velocity.x == 0:
+				velocity.x = -500
+			else:
+				velocity.x = -500
+		else:
+			velocity.x = 0
+	velocity.y += (gravidade * delta)
+	move_and_slide(velocity,normal)
+	pos_atual = get_pos()
+	if pos_atual.x > 705:
+		set_pos(Vector2(-62,pos_atual.y))
+	elif pos_atual.x < -65:
+		set_pos(Vector2(702,pos_atual.y))
+		
 	pass
